d0ab3e15991a0fb5be6b79a8f72a2695aa9c9dca
João Luiz Mineiro Alves <86202893+luizmineiro@users.noreply.github.com>
2024-04-04 21:45:05 -0300

    completando desafio

--- desafio.kt
+++ desafio.kt
@@ -2,20 +2,41 @@
 
 enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
 
-class Usuario
+class Usuario(var nome: String)
 
 data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
 
-data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
+data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>, var nivel: Nivel) {
 
     val inscritos = mutableListOf<Usuario>()
     
-    fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+    fun matricular(usuarios: Usuario) {
+        inscritos.addAll(usuarios)
+        for (usuario in usuarios) {
+            println("${usuario.nome} matriculado em $nome com sucesso.")
+        }
     }
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    val cursoAngular = ConteudoEducacional("Curso Angular 8+", 120)
+    val cursoSpring = ConteudoEducacional("Curso Spring Framework Completo", 240)
+    val cursoIntroducaoKotlin = ConteudoEducacional("Curso Kotlin Iniciante", 30)
+    val cursoJavaWeb = ConteudoEducacional("Curso Java Web", 120)
+    val cursoJs = ConteudoEducacional("JavaScript", 20)
+    val cursoAgile = ConteudoEducacional("Agile", 60)
+
+    val formacaoAngular = Formacao("Angular", listOf(cursoAngular), Nivel.INTERMEDIARIO)
+    val formacaoSpring = Formacao("Spring", listOf(cursoSpring), Nivel.INTERMEDIARIO)
+    val formacaoKotlin = Formacao("Curso Kotlin Iniciante", listOf(cursoIntroducaoKotlin), Nivel.INTERMEDIARIO)
+    val formacaoJavaWeb = Formacao("Java Web Fullstack", listOf(cursoJavaWeb), Nivel.INTERMEDIARIO)
+
+
+    println(formacaoAngular)
+    print("\n")
+    println(formacaoSpring)
+    print("\n")
+    println(formacaoJavaWeb)
+    print("\n")
+    println(formacaoKotlin)
 }
